55f2c4278bf1085af5f8fdd711ea016392a6f0d5
CAIDEN2606 <409854@edu.cwa.ac.uk>
2021-09-20 16:00:37 +0100

    start week 2 task

---
+++ 20thSepToDoTask.js
@@ -0,0 +1,34 @@
+function calculateTip(){
+    var billAmt = Number(document.getElementById("billamt").value);
+    var tipPercent = Number(document.getElementById("tipPercent").value);
+    var numOfPeople = Number(document.getElementById("peopleamt").value);
+
+    //validate input
+    if (billAmt === "" || tipPercent == 0) 
+    {
+        alert("Please enter values");
+        return;
+    }
+
+    //Check to see if this input is empty or less than or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } 
+    else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    //Calculate tip
+    var tip = billAmt * tipPercent;
+    var total = (tip+billAmt) / numOfPeople;
+    total = total.toFixed(2);
+    //Display the tip
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+  
+    //click to call function
+    document.getElementById("calculate").onclick = function() {
+    calculateTip();    
+    };
+}
